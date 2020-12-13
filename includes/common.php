5ae193455dba367467b2e88e0b50aa56b5ed7e0c
NikitaEmberi <59737567+NikitaEmberi@users.noreply.github.com>
2020-12-13 21:34:50 +0530

    Update common.php

--- includes/common.php
+++ includes/common.php
@@ -1,5 +1,6 @@
 <?php
-    $con = mysqli_connect("localhost", "root","", "banking_system")
+    $con = mysqli_connect("localhost", "root","", "banking_system") or die(mysqli_error($con));
+
     if(!isset($_SESSION)){
       session_start();
     }
