1a02bff67723f910c08415f1ed28e6df7a32cf78
NikitaEmberi <59737567+NikitaEmberi@users.noreply.github.com>
2020-12-13 21:28:40 +0530

    Update common.php

--- includes/common.php
+++ includes/common.php
@@ -1,7 +1,5 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "Nikita@123", "banking_system")
-    //$con = mysqli_connect("epiz_27040946_exp_manager", "epiz_27040946", "niki_3003", "epiz_27040946_exp_manager")
-    or die(mysqli_error($con));
+    $con = mysqli_connect("localhost", "root","", "banking_system")
     if(!isset($_SESSION)){
       session_start();
     }
