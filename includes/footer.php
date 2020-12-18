1a693543583561b4dbedab12fad86b1aa5421e3f
NikitaEmberi <59737567+NikitaEmberi@users.noreply.github.com>
2020-12-18 17:32:57 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -3,8 +3,8 @@
     <div class="container">
 		Copyright <span class="glyphicon glyphicon-copyright-mark"></span> Ct<span class="fa fa-inr"></span>l Transactions
         All Rights Reserved |
-        <button style="border-radius:5px;"><a href="https://github.com/NikitaEmberi"><img height=30 width=30 class="icons" src="https://www.flaticon.com/svg/static/icons/svg/2111/2111425.svg"></a></button>
-        <a target="_blank" href="https://www.linkedin.com/in/nikita-emberi-42a651200"><img height=30 width=30 class="icons" src="https://www.flaticon.com/svg/static/icons/svg/145/145807.svg"></a>
+        <a target="_blank" class="btn btn-social-icon btn-github" href="https://github.com/NikitaEmberi"><i class="fa fa-github"></i></a></button>
+        <a class="btn btn-social-icon btn-linkedin" target="_blank" href="https://www.linkedin.com/in/nikita-emberi-42a651200"><i class="fa fa-linkedin"></i></a>
         <a target="_blank" href="https://www.instagram.com/nikitaemberi/"><img height=30 width=30 class="icons" src="https://www.flaticon.com/svg/static/icons/svg/1409/1409946.svg"></a>
 
 	</div>
