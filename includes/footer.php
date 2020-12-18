96a1edf637d91917ae6e87fcd2c733080a7d17bc
NikitaEmberi <59737567+NikitaEmberi@users.noreply.github.com>
2020-12-18 17:34:20 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
                 All Rights Reserved |
         <a target="_blank" class="btn btn-social-icon btn-github" href="https://github.com/NikitaEmberi"><i class="fa fa-github"></i></a>
         <a class="btn btn-social-icon btn-linkedin" target="_blank" href="https://www.linkedin.com/in/nikita-emberi-42a651200"><i class="fa fa-linkedin"></i></a>
-        <a target="_blank" href="https://www.instagram.com/nikitaemberi/"><img height=30 width=30 class="icons" src="https://www.flaticon.com/svg/static/icons/svg/1409/1409946.svg"></a>
+        <a target="_blank" href="https://www.instagram.com/nikitaemberi/"><img height=30 width=30 class="icons" src="https://www.flaticon.com/svg/static/icons/svg/1409/1409946.svg"/></a>
 
 	</div>
 </center>
